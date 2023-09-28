33ad7f4860d34ca0c30ce986e2e6ff2786464202
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-09-28 17:37:33 -0400

    add cloudflare video download

--- src/lib/routes/create-transcription.ts
+++ src/lib/routes/create-transcription.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
-import { createReadStream } from "node:fs";
+import { createReadStream, createWriteStream, unlinkSync } from "node:fs";
 import { z } from "zod";
 import { prisma } from "../database";
 import { openai } from "../resources/openai";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Readable } from "node:stream";
+import path from "node:path";
 
 export async function CreateTranscription(app: FastifyInstance) {
   app.post("/:videoId/transcription", async (req) => {
@@ -24,6 +27,40 @@ export async function CreateTranscription(app: FastifyInstance) {
       },
     });
 
+    const S3 = new S3Client({
+      region: "auto",
+      endpoint: process.env.CLOUDFIRE_ENDPOINT ?? "",
+      credentials: {
+        accessKeyId: process.env.CLOUDFIRE_ACCESS_KEY_ID ?? "",
+        secretAccessKey: process.env.CLOUDFIRE_SECRET_ACCESS_KEY ?? "",
+      },
+    });
+
+    const downloadFile = await S3.send(
+      new GetObjectCommand({
+        Bucket: "bucket-audio-techvideo",
+        Key: video.uploadName,
+      })
+    );
+
+    const setTmpFile = new Promise(async (resolve, reject) => {
+      const body = downloadFile.Body;
+      if (body instanceof Readable) {
+        const uploadDir = path.resolve(
+          __dirname,
+          "../../tmp",
+          video.uploadName
+        );
+
+        body
+          .pipe(createWriteStream(uploadDir))
+          .on("error", (err) => reject(err))
+          .on("close", () => resolve("success"));
+      }
+    });
+
+    await setTmpFile;
+
     const videoPath = video.path;
     const audioReadStream = createReadStream(videoPath);
 
@@ -35,6 +72,8 @@ export async function CreateTranscription(app: FastifyInstance) {
       temperature: 0,
       prompt,
     });
+
+    unlinkSync(videoPath);
 
     const transcription = response.text;
 
