510e55ce08891f788ac4be41a82b7e511c38d1f1
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-09-30 19:53:42 -0400

    create generateAskMe route

--- src/lib/routes/routes.ts
+++ src/lib/routes/routes.ts
@@ -3,10 +3,12 @@ import { UploadVideo } from "./upload-video";
 import { CreateTranscription } from "./create-transcription";
 import { GenerateAiContent } from "./generate-ai-content";
 import { ListPrompts } from "./list-prompts";
+import { GenerateAskMe } from "./generate-ask-me";
 
 export async function Routes(app: FastifyInstance) {
   app.register(UploadVideo, { prefix: "/video" });
   app.register(CreateTranscription, { prefix: "/video" });
   app.register(GenerateAiContent, { prefix: "/ai" });
   app.register(ListPrompts, { prefix: "/prompts" });
+  app.register(GenerateAskMe, { prefix: "/prompt" });
 }
