1691c4cdd38fbb0127de9026ece5abba534178fa
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-10-23 00:12:11 -0400

    generate ai using task process and redis

--- src/lib/routes/app/generate-ai-content.ts
+++ src/lib/routes/app/generate-ai-content.ts
@@ -4,69 +4,74 @@ import { streamToResponse, OpenAIStream } from "ai";
 import { prisma } from "../../database";
 import { openai } from "../../resources/openai";
 import { delay } from "../../utils/delay";
+import { UpdateTaskById } from "../../resources/tasks";
 
 export async function GenerateAiContent(app: FastifyInstance) {
   app.post("/content", async (req, res) => {
-    const bodySchema = z.object({
-      videoId: z.string().uuid(),
-      generatorType: z.string(),
-      promptValue: z.string().optional(),
-      temperature: z.number().min(0).max(1).default(0.5),
-    });
+    const taskId = res.locals.taskId;
+    let response, error;
 
-    const { videoId, generatorType, temperature, promptValue } =
-      bodySchema.parse(req.body);
+    try {
+      const bodySchema = z.object({
+        videoId: z.string().uuid(),
+        generatorType: z.string(),
+        promptValue: z.string().optional(),
+        temperature: z.number().min(0).max(1).default(0.5),
+      });
 
-    const video = await prisma.video.findUniqueOrThrow({
-      where: {
-        id: videoId,
-      },
-    });
+      const { videoId, generatorType, temperature, promptValue } =
+        bodySchema.parse(req.body);
 
-    if (!video.transcription) {
-      return res
-        .status(400)
-        .send({ error: "Video transcription was not generated yet." });
-    }
-
-    let promptTemplate: string | undefined;
-
-    promptTemplate = promptValue;
-
-    if (generatorType !== "customize") {
-      const promptResult = await prisma.prompt.findFirst({
+      const video = await prisma.video.findUniqueOrThrow({
         where: {
-          title: generatorType,
+          id: videoId,
         },
       });
 
-      promptTemplate = promptResult?.template;
+      if (!video.transcription) {
+        return res
+          .status(400)
+          .send({ error: "Video transcription was not generated yet." });
+      }
+
+      let promptTemplate: string | undefined;
+
+      promptTemplate = promptValue;
+
+      if (generatorType !== "customize") {
+        const promptResult = await prisma.prompt.findFirst({
+          where: {
+            title: generatorType,
+          },
+        });
+
+        promptTemplate = promptResult?.template;
+      }
+
+      const promptMessage = promptTemplate?.replace(
+        "{transcription}",
+        video.transcription
+      );
+
+      if (!promptMessage)
+        return res.status(400).send({ error: "Prompt is not informed." });
+
+      res.send({ taskId });
+
+      await delay(3000);
+
+      const completionResult = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-16k",
+        temperature,
+        messages: [{ role: "user", content: promptMessage }],
+      });
+
+      response = { completion: completionResult.choices[0].message.content };
+    } catch (error) {
+      error = JSON.stringify(error);
+    } finally {
+      const result = await UpdateTaskById(app, taskId, response, error);
+      return res.send({ result });
     }
-
-    const promptMessage = promptTemplate?.replace(
-      "{transcription}",
-      video.transcription
-    );
-
-    if (!promptMessage)
-      return res.status(400).send({ error: "Prompt is not informed." });
-
-    await delay(3000);
-
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-16k",
-      temperature,
-      messages: [{ role: "user", content: promptMessage }],
-      stream: true,
-    });
-
-    const stream = OpenAIStream(response);
-
-    streamToResponse(stream, res.raw, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      },
-    });
   });
 }
